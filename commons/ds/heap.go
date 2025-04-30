e2ad68085411a622457025c2f5a277d1aa7d4834
Davide Olgiati <davide.olgiati@hotmail.it>
2025-04-30 11:01:29 +0200

    add heapify top down

--- commons/ds/heap.go
+++ commons/ds/heap.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Heap[T any] struct {
-	mutex sync.Mutex
-	items []T
-	custom_is_lower_fn func(T, T)bool
+	mutex              sync.Mutex
+	items              []T
+	custom_is_lower_fn func(T, T) bool
 }
 
 func Is_heap_empty[T any](heap *Heap[T]) bool {
@@ -34,12 +34,12 @@ func Push_into_heap[T any](heap *Heap[T], data T) {
 	heap.mutex.Lock()
 
 	heap.items = append(heap.items, data)
-	heapify(heap)
+	heapify_bottom_up(heap)
 
 	heap.mutex.Unlock()
 }
 
-func heapify[T any](heap *Heap[T]) {
+func heapify_bottom_up[T any](heap *Heap[T]) {
 	current_index := len(heap.items) - 1
 	parent := get_parent_node_index(current_index)
 	var swap_variable T
@@ -56,7 +56,38 @@ func heapify[T any](heap *Heap[T]) {
 	}
 }
 
-/*    
+func get_next_candidate[T any](heap *Heap[T], current int) int {
+	left := get_left_node_index(current)
+	right := get_right_node_index(current)
+
+	// BUG: cosa succede se left / right > len(items)
+
+	if heap.custom_is_lower_fn(heap.items[left], heap.items[right]) {
+		return left
+	} else {
+		return right
+	}
+}
+
+func heapify_top_down[T any](heap *Heap[T]) {
+	current_index := 0
+	candidate := get_next_candidate(heap, current_index)
+
+	var swap_variable T
+
+	for current_index < len(heap.items) && !heap.custom_is_lower_fn(heap.items[current_index], heap.items[candidate]) {
+		swap_variable = heap.items[candidate]
+		heap.items[candidate] = heap.items[current_index]
+		heap.items[current_index] = swap_variable
+
+		current_index = candidate
+		if current_index < len(heap.items) {
+			candidate = get_next_candidate(heap, current_index)
+		}
+	}
+}
+
+/*
 func Pop_from_heap[T any](heap *Heap[T]) T {
 	var item T
 	heap.mutex.Lock()
--- commons/ds/heap_test.go
+++ commons/ds/heap_test.go
@@ -7,29 +7,29 @@ func TestParentLeftRight(t *testing.T) {
 
 	expected_parent := 0
 	expected_left := 3
-	expected_right := 4 
+	expected_right := 4
 
 	actual_parent := get_parent_node_index(node_index)
 	actual_left := get_left_node_index(node_index)
 	actual_right := get_right_node_index(node_index)
-	
+
 	if actual_parent != expected_parent {
 		t.Errorf(
-			"get_parent_node_index(%d) = %d, expected %d", 
+			"get_parent_node_index(%d) = %d, expected %d",
 			node_index, actual_parent, expected_parent,
 		)
 	}
 
 	if actual_left != expected_left {
 		t.Errorf(
-			"get_left_node_index(%d) = %d, expected %d", 
+			"get_left_node_index(%d) = %d, expected %d",
 			node_index, actual_left, expected_left,
 		)
 	}
 
 	if actual_right != expected_right {
 		t.Errorf(
-			"get_right_node_index(%d) = %d, expected %d", 
+			"get_right_node_index(%d) = %d, expected %d",
 			node_index, actual_right, expected_right,
 		)
 	}
@@ -40,29 +40,29 @@ func TestParentLeftRightHead(t *testing.T) {
 
 	expected_parent := -1
 	expected_left := 1
-	expected_right := 2 
+	expected_right := 2
 
 	actual_parent := get_parent_node_index(node_index)
 	actual_left := get_left_node_index(node_index)
 	actual_right := get_right_node_index(node_index)
-	
+
 	if actual_parent != expected_parent {
 		t.Errorf(
-			"get_parent_node_index(%d) = %d, expected %d", 
+			"get_parent_node_index(%d) = %d, expected %d",
 			node_index, actual_parent, expected_parent,
 		)
 	}
 
 	if actual_left != expected_left {
 		t.Errorf(
-			"get_left_node_index(%d) = %d, expected %d", 
+			"get_left_node_index(%d) = %d, expected %d",
 			node_index, actual_left, expected_left,
 		)
 	}
 
 	if actual_right != expected_right {
 		t.Errorf(
-			"get_right_node_index(%d) = %d, expected %d", 
+			"get_right_node_index(%d) = %d, expected %d",
 			node_index, actual_right, expected_right,
 		)
 	}
@@ -75,24 +75,24 @@ func is_lower(a int, b int) bool {
 func TestHeapifyBase(t *testing.T) {
 	input_heap := Heap[int]{}
 
-	input_heap.items = []int{2,3,1}
+	input_heap.items = []int{2, 3, 1}
 	input_heap.custom_is_lower_fn = is_lower
-	
-	expected_array := []int{1,3,2,}
 
-	heapify(&input_heap)
+	expected_array := []int{1, 3, 2}
+
+	heapify_bottom_up(&input_heap)
 
 	if len(input_heap.items) != len(expected_array) {
 		t.Errorf(
-			"len(input_heap.items) = %d, expected %d", 
+			"len(input_heap.items) = %d, expected %d",
 			len(input_heap.items), 3,
 		)
 	}
-	
+
 	for i := 0; i < 3; i++ {
 		if input_heap.items[i] != expected_array[i] {
 			t.Errorf(
-				"input_heap.items[%d] = %d, expected %d", 
+				"input_heap.items[%d] = %d, expected %d",
 				i, input_heap.items[i], expected_array[i],
 			)
 		}
@@ -102,24 +102,24 @@ func TestHeapifyBase(t *testing.T) {
 func TestHeapifyLarge(t *testing.T) {
 	input_heap := Heap[int]{}
 
-	input_heap.items = []int{2,3,20,30,21,25,31,35,22,23,26,27,32,33,1}
+	input_heap.items = []int{2, 3, 20, 30, 21, 25, 31, 35, 22, 23, 26, 27, 32, 33, 1}
 	input_heap.custom_is_lower_fn = is_lower
-	
-	expected_array := []int{1,3,2,20,30,21,25,31,35,22,23,26,27,32,33}
 
-	heapify(&input_heap)
+	expected_array := []int{1, 3, 2, 20, 30, 21, 25, 31, 35, 22, 23, 26, 27, 32, 33}
+
+	heapify_bottom_up(&input_heap)
 
 	if len(input_heap.items) != len(expected_array) {
 		t.Errorf(
-			"len(input_heap.items) = %d, expected %d", 
+			"len(input_heap.items) = %d, expected %d",
 			len(input_heap.items), 3,
 		)
 	}
-	
+
 	for i := 0; i < 3; i++ {
 		if input_heap.items[i] != expected_array[i] {
 			t.Errorf(
-				"input_heap.items[%d] = %d, expected %d", 
+				"input_heap.items[%d] = %d, expected %d",
 				i, input_heap.items[i], expected_array[i],
 			)
 		}
