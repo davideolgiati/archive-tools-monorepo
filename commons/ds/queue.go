9c2b1f87fc1d6356041ba445efa55265b154400c
Davide Olgiati <davide.olgiati@hotmail.it>
2025-06-06 15:47:51 +0200

    Refactor Queue implementation: improve Push and Pop methods; add resize functionality; update build timestamp and version

--- commons/ds/queue.go
+++ commons/ds/queue.go
@@ -2,27 +2,52 @@ package ds
 
 type Queue[T comparable] struct {
 	queue []T
+	head  int
+	tail  int
+	size  int
 }
 
 func (queue *Queue[T]) Init() {
-	queue.queue = make([]T, 0)
+	queue.queue = make([]T, 30)
+	queue.head = 0
+	queue.tail = 0
+	queue.size = 0
 }
 
 func (queue *Queue[T]) Push(value T) {
-	queue.queue = append(queue.queue, value)
+	if queue.size == len(queue.queue) {
+		queue.resize()
+	}
+	
+	queue.queue[queue.tail] = value
+	queue.tail = (queue.tail + 1) % len(queue.queue)
+	queue.size++
 }
 
 func (queue *Queue[T]) Pop() T {
-	if len(queue.queue) == 0 {
-		panic("Pop on empty list")
+	if queue.size == 0 {
+		panic("Pop on empty queue")
 	}
 
-	current := queue.queue[0]
-	queue.queue = queue.queue[1:]
+	value := queue.queue[queue.head]
+	queue.head = (queue.head + 1) % len(queue.queue)
+	queue.size--
 
-	return current
+	return value
 }
 
 func (queue *Queue[T]) Empty() bool {
-	return len(queue.queue) == 0
+	return queue.size == 0
+}
+
+func (queue *Queue[T]) resize() {
+	newQueue := make([]T, len(queue.queue)*2)
+	
+	for i := 0; i < queue.size; i++ {
+		newQueue[i] = queue.queue[(queue.head+i)%len(queue.queue)]
+	}
+	
+	queue.queue = newQueue
+	queue.head = 0
+	queue.tail = queue.size
 }
