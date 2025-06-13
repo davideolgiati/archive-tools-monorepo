6a69de7d7b258a12229bcb7cf274c472e5f1f0d1
Davide Olgiati <davide.olgiati@hotmail.it>
2025-06-13 17:41:21 +0200

    Refactor WriteOnlyThreadPool: restructure internal types for better organization, enhance worker function setup, and improve thread management; update build timestamp and version

--- commons/thread_pool.go
+++ commons/thread_pool.go
@@ -8,14 +8,26 @@ import (
 	"time"
 )
 
+type poolConfiguration[T any] struct {
+	workerFunction func(*poolSharedResources[T])
+	maxWorkers     int
+}
+
+type poolStatus struct {
+	poolLoad      []float64
+	activeThreads atomic.Int64
+	isClosed      bool
+}
+
+type poolSharedResources[T any] struct {
+	inputChannel chan T
+	waitingGroup sync.WaitGroup
+}
+
 type WriteOnlyThreadPool[T any] struct {
-	inputChannel          chan T
-	workerFn              func(chan T, *sync.WaitGroup)
-	workloadFactorSamples []float64
-	waitingGroup          sync.WaitGroup
-	maxWorkers            int
-	activeTasks           atomic.Int64
-	closed                bool
+	configuration poolConfiguration[T]
+	status        poolStatus
+	shared        poolSharedResources[T]
 }
 
 func NewWorkerPool[T any](workerFn func(T)) (*WriteOnlyThreadPool[T], error) {
@@ -36,16 +48,18 @@ func NewWorkerPool[T any](workerFn func(T)) (*WriteOnlyThreadPool[T], error) {
 		return threadPool, fmt.Errorf("error while looking for CPU info in threadpool setup")
 	}
 
-	threadPool.maxWorkers = workersCount
-	threadPool.inputChannel = inputChannel
-	threadPool.workloadFactorSamples = sampleArray
-	threadPool.workerFn = setupWorkerFunction(workerFn, &threadPool.activeTasks)
-	threadPool.closed = false
+	threadPool.status = poolStatus{}
+	threadPool.status.activeThreads.Store(0)
+	threadPool.status.isClosed = false
+	threadPool.status.poolLoad = sampleArray
 
-	threadPool.activeTasks = atomic.Int64{}
-	threadPool.activeTasks.Store(0)
+	threadPool.configuration = poolConfiguration[T]{}
+	threadPool.configuration.maxWorkers = workersCount
+	threadPool.configuration.workerFunction = setupWorkerFunction(workerFn, &threadPool.status.activeThreads)
 
-	for i := 0; i < threadPool.maxWorkers; i++ {
+	threadPool.shared.inputChannel = inputChannel
+
+	for i := 0; i < threadPool.configuration.maxWorkers; i++ {
 		threadPool.addNewWorker()
 	}
 
@@ -53,13 +67,13 @@ func NewWorkerPool[T any](workerFn func(T)) (*WriteOnlyThreadPool[T], error) {
 }
 
 func (tp *WriteOnlyThreadPool[T]) Submit(data T) error {
-	if tp.closed {
+	if tp.status.isClosed {
 		return fmt.Errorf("send on closed thread pool")
 	}
 
 	select {
-	case tp.inputChannel <- data:
-		tp.activeTasks.Add(1)
+	case tp.shared.inputChannel <- data:
+		tp.status.activeThreads.Add(1)
 		return nil
 	case <-time.After(time.Second * 3):
 		return fmt.Errorf("submit timeout - workers may be overloaded")
@@ -67,29 +81,29 @@ func (tp *WriteOnlyThreadPool[T]) Submit(data T) error {
 }
 
 func (tp *WriteOnlyThreadPool[T]) Release() {
-	tp.closed = true
+	tp.status.isClosed = true
 
 	tp.Wait()
-	close(tp.inputChannel)
+	close(tp.shared.inputChannel)
 
-	tp.waitingGroup.Wait()
+	tp.shared.waitingGroup.Wait()
 }
 
 func (tp *WriteOnlyThreadPool[T]) Wait() {
-	for tp.activeTasks.Load() > 0 {
+	for tp.status.activeThreads.Load() > 0 {
 		time.Sleep(1 * time.Millisecond)
 	}
 }
 
-func setupWorkerFunction[T any](fn func(T), activeTasks *atomic.Int64) func(chan T, *sync.WaitGroup) {
-	return func(in chan T, wg *sync.WaitGroup) {
-		if wg == nil {
-			panic("waitgroup pointer is nil")
+func setupWorkerFunction[T any](fn func(T), activeTasks *atomic.Int64) func(*poolSharedResources[T]) {
+	return func(shared *poolSharedResources[T]) {
+		if shared == nil {
+			panic("poolSharedResources pointer is nil")
 		}
 
-		defer wg.Done()
+		defer shared.waitingGroup.Done()
 
-		for obj := range in {
+		for obj := range shared.inputChannel {
 			fn(obj)
 			activeTasks.Add(-1)
 		}
@@ -97,6 +111,6 @@ func setupWorkerFunction[T any](fn func(T), activeTasks *atomic.Int64) func(chan
 }
 
 func (tp *WriteOnlyThreadPool[T]) addNewWorker() {
-	tp.waitingGroup.Add(1)
-	go tp.workerFn(tp.inputChannel, &tp.waitingGroup)
+	tp.shared.waitingGroup.Add(1)
+	go tp.configuration.workerFunction(&tp.shared)
 }
